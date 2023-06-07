7871cf78f289b1ef93e79edc8e891da85cf09b8f
Miguel Carvalho <doxito007@gmail.com>
2023-06-07 19:31:39 -0300

    Adicionando o set do EJS

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import BodyParser from "body-parser";
+import path from "path";
+
 import router from "./routes";
 
 const app = express();
@@ -9,6 +11,10 @@ app.use(express.json());
 
 
 app.use(router);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Ouvindo a porta 3000");
